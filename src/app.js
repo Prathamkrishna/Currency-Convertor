530e37a2e1e6fcf85d7c8153a01567e7e5721374
Pratham Krishna <pratham.krishna.06@gmail.com>
2021-05-07 08:56:52 +0530

    added axios to app.js, a few changes in index.js

--- src/app.js
+++ src/app.js
@@ -1,17 +1,44 @@
-import React, { useEffect } from "react";
+import React from "react";
+import axios from 'axios';
 
 const API_KEY = `d0f777c796f1003bc1a7ef7b77863799`;
+const endpoint = 'convert';
+const INR = 'INR';
 
-function CurrencyConv() {
-	useEffect(() => {
-		fetch(`http://api.currencylayer.com/live?access_key=${API_KEY}`, {
-			method: "GET",
-		})
-			.then((res) => res.json())
-			.then((res) => {
-				console.log({ res });
-			});
-	}, []);
+// axios({
+// 	method: 'get',
+// 	url: 'http://api.currencylayer.com',
+// 	responseType: 'stream'	
+// })
+// 	.then(function(response){
+// 		response.data.pipe(fs.create)
+// 	})
+// axios('/'+endpoint+'?access_key='+API_KEY+'&from='+props+'&to='+INR+'&amount='+amount)
+
+function CurrencyConv(props) {
+//   useEffect(() => {
+//     fetch('http://api.currencylayer.com/' + endpoint + '?access_key=' + API_KEY + '&from=' + props + '&to=' + INR + '&amount=' + amount, {
+//       method: "GET",
+//     })
+//       .then((res) => res.json())
+//       .then((res) => {
+//         console.log({ res });
+//       });
+//     console.log(props);
+// 	console.log(amount);
+//   }, [props, amount]);
+//   console.log(res\\);
+	console.log(props.currency)
+	console.log(props.amount)
+	axios.get({
+		url: 'http://api.currencylayer.com/' + endpoint + '?access_key=' + API_KEY + '&from=' + props.currency + '&to=' + INR + '&amount=' + props.amount
+	})
+	.then(function(response){
+		console.log(response);
+	})
+	.catch(function (error) {
+		console.log(error);
+	  })
 	return <div>Currency Converter</div>;
 }
 
--- src/index.js
+++ src/index.js
@@ -1,62 +1,88 @@
-import React, {useState} from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import CurrencyCalc from './app.js'
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import CurrencyCalc from "./app.js";
+
+const CURRENCIES_MAP = [
+  {
+    abbr: "USD",
+    name: "US Dollar",
+  },
+  {
+    abbr: "GBP",
+    name: "British Pounds",
+  },
+  {
+    abbr: "INR",
+    name: "Indian Rupee",
+  },
+  {
+    abbr: "AUD",
+    name: "Australian Dollar",
+  },
+];
 
 function Main() {
-    const [currency, setCurrency] = useState('USD');
-    const [amount, setAmount] = useState({eventCount: 0, Amount: ''})
-    // const endpoint = 'convert';
-    function amountChange(event){
-        setAmount({Amount: event.target.value});
-    }
-    function selectCurrency(x){
-        if(x === "USD"){
-            setCurrency('USD');
-            // const from = 'USD';
-            // const to = 'INR';
-            // const amountt = `{amount.Amount}`
-        }
-        if(x === "Pounds"){
-            setCurrency('Pounds');
-        }
-        if(x === "INR"){
-            setCurrency('Indian Rupees')
-        }
-        if(x === "AUS"){
-            setCurrency('Australian Dollars')
-        }
-    }
-    // $.ajax({
-        url: 'https://api.currencylayer.com/' + endpoint + '?access_key=' + access_key +'&from=' + from + '&to=' + to + '&amount=' + amount,
-    //     dataType: 'jsonp',
-    //     success: function(json){
-    //         alert(json.result);
-    //     }
-    // });
-    return(
-        <div>
-            Enter currency: <input type="number" onChange={amountChange}/>
-            <br />
-            <div className='optionss'>
-                <select>
-                    <option value="Dollars" onClick={()=>selectCurrency("USD")}>USD</option>
-                    <option value="Pounds" onClick={()=>selectCurrency("GBP")}>GBP</option>
-                    <option value="inr" onClick={()=>selectCurrency("INR")}>INR</option>
-                    <option value="aus" onClick={()=>selectCurrency("AUD")}>AUD</option>
-                </select>
-            </div>
-            <br />
-            <div>
-                The amount entered is {amount.Amount} {currency}.
-            </div>
-            <br />
-            <div>
-                <CurrencyCalc />
-                
-            </div>
-        </div>
-    )
+  const [currency, setCurrency] = useState("USD");
+  const [amount, setAmount] = useState({ eventCount: 0, Amount: "0" });
+  // const endpoint = 'convert';
+  function amountChange(event) {
+    setAmount({ Amount: event.target.value });
+  }
+
+  function selectCurrency(currencyAbbr) {
+    setCurrency(currencyAbbr);
+  }
+  //     if (x === "USD") {
+  //       setCurrency("USD");
+  //       // const from = 'USD';
+  //       // const to = 'INR';
+  //       // const amountt = `{amount.Amount}`
+  //     }
+  //     if (x === "Pounds") {
+  //       setCurrency("Pounds");
+  //     }
+  //     if (x === "INR") {
+  //       setCurrency("Indian Rupees");
+  //     }
+  //     if (x === "AUS") {
+  //       setCurrency("Australian Dollars");
+  //     }
+  //   }
+  // $.ajax({
+  // url: 'https://api.currencylayer.com/' + endpoint + '?access_key=' + access_key +'&from=' + from + '&to=' + to + '&amount=' + amount,
+  //     dataType: 'jsonp',
+  //     success: function(json){
+  //         alert(json.result);
+  //     }
+  // });
+  return (
+    <div>
+      Enter currency: <input type="number" onChange={amountChange} />
+      <br />
+      <div className="optionss">
+        <select>
+          {CURRENCIES_MAP.map((currencyObj) => (
+            <option
+              value={currencyObj.name}
+              onClick={() => selectCurrency(currencyObj.abbr)}
+              key={currencyObj.abbr}
+            >
+              {`${currencyObj.name} — ${currencyObj.abbr}`}
+            </option>
+          ))}
+        </select>
+      </div>
+      <br />
+      <div>
+        The amount entered is {amount.Amount} {currency}.
+      </div>
+      <br />
+      <div>
+        <CurrencyCalc currency={currency} amount={amount.Amount}/>
+      </div>
+    </div>
+  );
 }
 
-ReactDOM.render( < Main /> , document.getElementById('root'))
+ReactDOM.render(<Main />, document.getElementById("root"));
